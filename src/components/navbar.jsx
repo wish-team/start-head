188dbb8cdce465275dfb7ee46680075786845bef
amirhoseinsh <amirhhh.shirani@yahoo.com>
2024-10-15 14:02:40 +0330

    navbar mobile device

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -21,37 +21,41 @@ export default function Navigationbar() {
       <Navbar
         fluid={true}
         rounded={true}
-        className="relative ml-2 bg-white bg-opacity-10 fixed backdrop-blur-lg border border-white border-opacity-20 shadow-md rounded-lg px-4 md:px-8 py-2 md:py-4"
+        className="relative ml-2 bg-white bg-opacity-10 fixed h-auto backdrop-blur-lg border border-white border-opacity-20 shadow-md rounded-lg px-4 md:px-8 py-2 md:py-4"
       >
         {/* Hamburger Icon for Mobile Screens */}
         <button
           className="md:hidden text-white focus:outline-none z-50"
           onClick={handleToggle}
         >
-          <i className="fa fa-bars fa-lg"></i>
+          {isOpen ? (
+            <i className="fa fa-times fa-lg"></i> // Close icon (X)
+          ) : (
+            <i className="fa fa-bars fa-lg"></i> // Hamburger icon
+          )}
         </button>
 
         {/* Mobile Menu: shown when the hamburger is clicked */}
         {isOpen && (
-          <div className="top-12 left-0 w-full shadow-md z-40">
-            <Navbar.Collapse className="flex flex-col gap-4">
+          <div className="top-12 left-0 w-full h-screen z-40">
+            <Navbar.Collapse className="flex flex-col gap-4 overflow-auto">
               <Navbar.Link
                 href="/vision"
-                className={`px-4 py-2 rounded-full ${activePath === '/vision' ? 'text-primary bg-[#1e1e1e]' : 'text-white'} transition-all`}
+                className={`px-4 py-2 rounded-sm border-none hover:text-primary ${activePath === '/vision' ? 'text-primary bg-[#1e1e1e]' : 'text-white'} transition-all`}
                 style={{ margin: '0.5rem 1rem' }}
               >
                 Vision
               </Navbar.Link>
               <Navbar.Link
                 href="/"
-                className={`px-4 py-2 rounded-full ${activePath === '/' ? 'text-primary bg-[#1e1e1e]' : 'text-white'} transition-all`}
+                className={`px-4 py-2 rounded-sm border-none ${activePath === '/' ? 'text-primary bg-[#1e1e1e]' : 'text-white'} transition-all`}
                 style={{ margin: '0.5rem 1rem' }}
               >
                 Home
               </Navbar.Link>
               <Navbar.Link
                 href="/team"
-                className={`px-4 py-2 rounded-full ${activePath === '/team' ? 'text-primary bg-[#1e1e1e]' : 'text-white'} transition-all`}
+                className={`px-4 py-2 rounded-sm border-none ${activePath === '/team' ? 'text-primary bg-[#1e1e1e]' : 'text-white'} transition-all`}
                 style={{ margin: '0.5rem 1rem' }}
               >
                 Team
